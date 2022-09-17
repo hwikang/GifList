d6d616bbe6745f9676888dd260eb7919f948add5
hwikang <khdrogba@gmail.com>
2022-09-17 14:02:05 +0900

    init

---
+++ kakaohairshop/kakaohairshop/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  kakaohairshop
+//
+//  Created by 슈퍼 on 2022/09/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
